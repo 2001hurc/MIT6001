06f8bc732125009ea034a6763736650a18ef9388
2001hurc <hurc2001@gmail.com>
2018-09-01 19:57:47 +0800

    添加一些原来的笔记和代码

---
+++ lec_2/lec_2_1.py
@@ -0,0 +1,8 @@
+#while
+n = input("You're in the Lost Forest. Go left or right? ")
+while n == "right":
+    n = input("You're in the Lost Forest. Go left or right? ")
+    if n == 
+print("You got out of the Lost Forest!")
+
+
---
+++ lec_2/lec_2_2.py
@@ -0,0 +1,19 @@
+#while循环
+'''
+n = int(input('请输数字:'))
+while n < 5:
+    print(n)
+    n = n + 1
+
+for n in range(5):
+    print(n)
+
+'''
+'''
+mysum = 0
+for n in range(2,10,3):
+    mysum += n
+    print(mysum)
+
+'''
+
